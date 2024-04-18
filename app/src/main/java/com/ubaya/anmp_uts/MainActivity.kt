b2b999fbbba783d60bb5abe4bda8e81a5091812d
IMRMT <michaeltimonuli@gmail.com>
2024-04-19 00:05:30 +0700

    proses profil update

--- app/src/main/java/com/ubaya/anmp_uts/MainActivity.kt
+++ app/src/main/java/com/ubaya/anmp_uts/MainActivity.kt
@@ -2,13 +2,32 @@ package com.ubaya.anmp_uts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.ubaya.anmp_uts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    val fragment: ArrayList<Fragment> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fragment.add(PrefFragment())
+
+        binding.viewPager.adapter = MyAdapter(this, fragment)
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                binding.bottomNav.selectedItemId = binding.bottomNav.menu.getItem(position).itemId
+            }
+        })
+
+        binding.bottomNav.setOnItemSelectedListener {
+            binding.viewPager.currentItem = when(it.itemId){
+                R.id.itemHome -> 0
+                R.id.itemPref -> 1
+                else -> 0
+            }
+            true
+        }
     }
 }
